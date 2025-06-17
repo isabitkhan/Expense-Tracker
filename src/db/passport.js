3c1a791e9f60f695b11594e4971fb4bfeeb7422e
isabitkhan <mesabitkhan@gmail.com>
2025-06-17 15:51:32 +0500

    update callbackUrl

--- src/db/passport.js
+++ src/db/passport.js
@@ -9,9 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // callbackURL: process.env.GOOGLE_CALLBACK_URL,
-      callbackURL: "http://localhost:8000/api/v1/auth/google/callback",
-
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       // http://localhost:8000/api/v1/auth/google/,
     },
     async (accessToken, refreshToken, profile, done) => {
